1c8d8f0a4b9dd59ffd4bb5a281a91deff969494d
Yadunandan Batchu <yadunandanbatchu@Yadunandans-iMac.local>
2018-05-03 17:45:50 +0530

    question handling

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -2,23 +2,28 @@ import { h, Component } from 'preact';
 
 import { auth, database } from '../firebase';
 
-export default class Game extends Component {
+export default class question extends Component {
     constructor(){
         super();
 
         this.state = {
-            game: {
-                q: "before"
+            answerLock: false,
+            question: {
+                id: 0,
+                q: "Question placeholder",
+                a: "Option A",
+                b: "Option B",
+                c: "Option C"
             }
         }
     }
 
     componentDidMount(){
-        const game = database.ref('/' + 'game');
+        const question = database.ref('/' + 'question');
 
-        game.on('value', snapshot => {
-            this.setState({ game: snapshot.val() });
-            console.log(this.state.game);
+        question.on('value', snapshot => {
+            this.setState({ question: snapshot.val(), answerLock: false });
+            console.log(this.state.question);
         });
     }
 
@@ -26,11 +31,32 @@ export default class Game extends Component {
         auth.signOut()
     }   
 
+    select(e) {
+        if (this.state.answerLock) { return }
+
+        const user = database.ref('/users/'+ auth.currentUser.uid );
+        const responses = user.child("responses");
+
+        const answer = {};
+        answer[this.state.question.id] = e.target.id;
+
+        responses.update(answer, () => {
+            this.setState({answerLock: true})
+            console.log("answered");
+        })
+    }
+
     render() {
         
         return (
             <div>
-                {this.state.game.q}
+                {this.state.question.q}
+                <ul>
+                    <li id="a" onClick={this.select.bind(this)} >{this.state.question.a}</li>
+                    <li id="b" onClick={this.select.bind(this)} >{this.state.question.b}</li>
+                    <li id="c" onClick={this.select.bind(this)} >{this.state.question.c}</li>
+                </ul>
+                
                 <br/>
                 <button onClick={this.logout.bind(this)} >Logout</button>
             </div>
